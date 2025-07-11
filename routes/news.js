4102eff12941ff20ca5d5718d52d5bebf1fd4b5a
amok75 <artur.mokwinski@gmail.com>
2025-07-11 14:10:45 +0200

    news sort

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const News = require("../models/news");
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("news", { title: "News" });
+router.get("/", async (req, res, next) => {
+  const search = req.query.search || "";
+
+  const findNews = News.find({ title: new RegExp(search.trim(), "i") }).sort({
+    created: -1,
+  });
+
+  const data = await findNews.exec();
+
+  res.render("news", { title: "News", data, search });
 });
 
 module.exports = router;
